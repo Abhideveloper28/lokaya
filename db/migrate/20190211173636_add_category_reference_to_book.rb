fdcffca4a6fcafedef49ae6f81cb79dfc06ba871
abhideveloper28 <abhi.developer28@gmail.com>
2019-02-11 23:07:25 +0530

    Category model

---
+++ db/migrate/20190211173636_add_category_reference_to_book.rb
@@ -0,0 +1,5 @@
+class AddCategoryReferenceToBook < ActiveRecord::Migration[5.2]
+  def change
+    add_reference :books, :category, foreign_key: true
+  end
+end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_02_11_173356) do
+ActiveRecord::Schema.define(version: 2019_02_11_173636) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -27,8 +27,17 @@ ActiveRecord::Schema.define(version: 2019_02_11_173356) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.bigint "author_id"
+    t.bigint "category_id"
     t.index ["author_id"], name: "index_books_on_author_id"
+    t.index ["category_id"], name: "index_books_on_category_id"
+  end
+
+  create_table "categories", force: :cascade do |t|
+    t.string "name"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
   end
 
   add_foreign_key "books", "authors"
+  add_foreign_key "books", "categories"
 end
